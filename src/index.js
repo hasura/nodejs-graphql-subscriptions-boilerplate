781b965d60d18097b6245b150c2d9e54f7e5dcce
Tanmai Gopal <tanmaig@hasura.io>
2018-07-25 20:12:05 +0530

    WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Subscribe = require('./subscribe');
 const wsurl = process.env.GRAPHQL_URL;
 
-const subscribeLiveQueries= `
+const subscribeQuery = `
 subscription liveAuthor {
 	author {
 		id
@@ -15,12 +15,16 @@ function main() {
   // wsurl: GraphQL url
   // query: Initial GraphQL query
   // variables: Variables if any
-	const subscriptionInstance = Subscribe.subscribe(wsurl, subscribeLiveQueries, {});
+	const subscriptionInstance = Subscribe.subscribe(
+		wsurl, // GraphQL URL
+		subscribeLiveQueries, // Subscription query 
+		{} // Query variables
+	);
   
-	subscriptionInstance.start();
+  // open the websocket connection with the upstream (GraphQL server)
+  subscriptionInstance.start();
   console.log('Started watching for live query on author table');
 
-  // open the websocket connection with the upstream (GraphQL server)
   // and returns an observer which can be used to end the connection later.
   // Callback will be called whenever there is new data available from the upstream
 	var observer = subscriptionInstance.executable.subscribe(eventData => {
